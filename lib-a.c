ab205b115d2e30c8c1a66a70e2cce9eea482cce1
Eugene Petrenko <eugene.petrenko@gmail.com>
2018-06-11 15:38:24 +0200

    lib a and lib b:

---
+++ lib-a.c
@@ -0,0 +1,8 @@
+#include "lib-a.h"
+
+
+int lib_a_foo() {
+  return 42;
+}
+
+
---
+++ lib-b.c
@@ -0,0 +1,11 @@
+#include "lib-b.h"
+#include "lib-a.h"
+
+
+int lib_b_foo() {
+
+  return 42 + lib_a_foo();;
+
+}
+
+
